3429242241f9a7764eb2ecd79c26dfeaeb5edfe7
Александр Калашников <alexdenkk@yandex.ru>
2024-03-31 20:31:48 +0700

    Add files via upload

---
+++ server.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+
+import Yesod
+
+import System.Random
+import Control.Monad (replicateM)
+
+data Server = Server
+
+-- routes
+mkYesod "Server" [parseRoutes|
+/ Index GET
+/testapi TestApi GET
+|]
+
+instance Yesod Server
+
+
+
+-- handlers
+getIndex = do
+    defaultLayout [whamlet|<h1 align="center">Welcome</h1>|]
+
+getTestApi = do
+    return $ object ["phrase" .= "Whatever happens, happens..."]
+
+main :: IO ()
+main = do
+    warp 8000 Server -- run server
